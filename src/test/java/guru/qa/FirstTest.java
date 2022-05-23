3d3f182a749739ab554b54779b2413fd94bf6130
Dmitrii Tuchs <dtuchs@gmail.com>
2022-05-24 00:52:07 +0700

    first commit

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
